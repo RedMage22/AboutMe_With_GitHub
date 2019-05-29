7cf0bd258831509fefcf65593f248b884dd7fd27
RedMage <rsonic2@gmail.com>
2019-05-29 21:49:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! My name is Rodney!");
+        System.out.println("I like to program, game and tinker with my computer.");
+        System.out.println("I have been programming in Java since 2012.");
+
+    }
+}

20cc5acad9884c8ed8aff3f28e60ce0306677b34
RedMage <rsonic2@gmail.com>
2019-05-29 21:59:53 +0200

    adding a comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        // Using println
         System.out.println("Hi! My name is Rodney!");
         System.out.println("I like to program, game and tinker with my computer.");
         System.out.println("I have been programming in Java since 2012.");
